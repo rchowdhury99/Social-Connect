7a54576c1bd1dfc3afe0135b6b65853bf5ac7800
Shrek <faheem5948@gmail.com>
2019-02-23 05:44:30 -0500

    added a link to the login page from the index/home page

--- WEB/main.py
+++ WEB/main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route("/login/")
+def login():
+    return render_template("login.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
