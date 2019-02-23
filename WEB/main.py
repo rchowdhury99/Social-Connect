2a510663ec87cd189be52b5cfa79670e705bd60a
Shrek <faheem5948@gmail.com>
2019-02-22 23:05:59 -0500

    starting file
    flask starting template

---
+++ WEB/main.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "WORKS"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
