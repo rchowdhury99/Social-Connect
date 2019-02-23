6ac60e1d6c2a0c021ec0ddc05ba44b95dfcba5d5
Shrek <faheem5948@gmail.com>
2019-02-23 12:34:35 -0500

    Fixed the login error, get the social links
    - fixed byte string & unicode errors

--- WEB/helper.py
+++ WEB/helper.py
@@ -10,31 +10,24 @@ class Database(object):
         cursor = connect.execute("SELECT Email, Password FROM %s WHERE EMAIL == '%s'" % (db_table, email))
         # password for the email if exists
         password = None
+        # getting the password from the query
         for i in cursor:
             for item in i:
                 if item.encode("ascii") != email:
-                    password = item.encode("ascii")
+                    password = (item.encode("ascii")).decode("utf-8")
+
 
         print("DB-Password: %s" % (password))
         return password
 
 
-    def get(self, db_name, db_table):
-        '''
-        Returns table rows given a table
-        Args: db_name, db_table
-        Returns: columns(list), rows(list)
-        '''
+    def get_social_links(self, db_name, db_table, email):
         connect = sqlite3.connect(db_name)
-        cursor = connect.cursor()
-        # getting rows
-        x = cursor.execute("SELECT * FROM %s" % (db_table)).fetchall()
-        # getting columns
-        columns = list(map(lambda x: x[0], cursor.description))
-        # seperating rows
-        rows = []
-        for i in x:
-            rows.append(list(i))
+        cursor = connect.execute("SELECT Email, Facebook, Twitter, Instagram, Snapchat FROM %s WHERE Email == %s" % (db_table, email))
+        for i in cursor:
+            print(i)
+            for item in i:
+                if (item.encode("ascii")).decode("utf-8") != email:
+                    social_links.append((item.encode("ascii")).decode("utf-8"))
 
-        # returning rows and columns
-        return [columns, rows]
+        return social_links
--- WEB/test.py
+++ WEB/test.py
@@ -1,10 +1,15 @@
 import sqlite3
 
-connection = sqlite3.connect("db.db")
-x = connection.execute("SELECT Email, Password FROM CLIENTS WHERE Email == 'john.doe@example.com'");
-password = None
-for i in x:
+db_table = "CLIENTS"
+email = "john.doe@example.com"
+connect = sqlite3.connect("db.db")
+cursor = connect.execute("SELECT Email, FacebookID, TwitterID, InstagramID, SnapchatID FROM %s WHERE Email == '%s'" % (db_table, email))
+social_links = []
+# getting the social links from the query
+for i in cursor:
     print(i)
-    password = list(i)
-    password.remove("john.doe@example.com")
-print(password)
+    for item in i:
+        if (item.encode("ascii")).decode("utf-8") != email:
+            social_links.append((item.encode("ascii")).decode("utf-8"))
+
+print(social_links)
