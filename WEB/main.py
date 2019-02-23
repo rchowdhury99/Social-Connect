ce9944b9c7ef04c0554584c760237fd0b6412841
Reizwan <reizchowdhury@gmail.com>
2019-02-23 01:52:05 -0500

    Added bootstrap

--- WEB/main.py
+++ WEB/main.py
@@ -1,13 +1,13 @@
 import os
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def main():
-    return "WORKS"
-
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
